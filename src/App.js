ea556f90c416a998fe5561ac033260fb074f4147
Zephyr Mann <zephmann@gmail.com>
2021-04-04 23:02:55 -0400

    Begin updating react app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // const [server, setServer] = useState(0);
+  // const [opponent, setOpponent] = useState(0);
+  // const [score, setScore] = useState("");
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      server: 1,
+      opponent: 0,
+      score: ""
+    };
+  }
+
+  componentDidMount() {
+    const url = "/score?server=0&opponent=0";
+    fetch(url).then(res => res.json()).then(data => {
+      this.setState({ score: data.score })
+      // setScore(data.score);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Tennis Score</h1>
+        <h1 id="score">"{this.state.score}"</h1>
+        <div id="server" className="btn"><h2>Point Server</h2></div>
+        <div id="opponent" className="btn"><h2>Point Opponent</h2></div>
+      </div>
+    );
+  }
 }
 
 export default App;
