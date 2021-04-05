ded47dc82f9ba2ad19d2e7968fb53df4f2560d42
Zephyr Mann <zephmann@gmail.com>
2021-04-05 14:12:01 -0400

    First pass at setting up app for deployment.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  // const [server, setServer] = useState(0);
-  // const [opponent, setOpponent] = useState(0);
-  // const [score, setScore] = useState("");
 
   constructor(props) {
     super(props);
     
     this.state = {
-      server: 1,
+      server: 0,
       opponent: 0,
       score: ""
     };
   }
 
   componentDidMount() {
-    const url = "/score?server=0&opponent=0";
+    const url = "/api/score?server=0&opponent=0";
     fetch(url).then(res => res.json()).then(data => {
       this.setState({ score: data.score })
-      // setScore(data.score);
     });
   }
 
